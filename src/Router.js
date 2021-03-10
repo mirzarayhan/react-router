8879ebf1190d4b3c65e1072b2fe663bbc37bdf4d
Mirza Rayhan <mirzarayhan22@gmail.com>
2021-03-10 20:17:46 +0700

    Praktikum 1 : React-Router

---
+++ src/Router.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+      /** Router berfungsi untuk Router merupakan modul yang berfungsi untuk melakukan proses navigasi pada SPA(Single page application)*/
+      /** Link iberfungsi sebagai pengganti tag <a> pada html, kelebihannya adalah  menghubungkan langsung anchor kita ke config router di root component yang dibuat buat */
+      // situs memiliki 3 halaman yaitu home about dan users
+      // halaman tidak perlu direfresh
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
